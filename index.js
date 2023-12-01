9f7280baf52ba2c061dbb2cfdd7a21a335850190
Krithik Singh <krithiksingh@gmail.com>
2023-12-01 12:23:20 -0500

    working fetch request made

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('we are connected!!!');
+    fetchData();
 });
+
+function fetchData() {
+    fetch('http://localhost:3000/expenses')
+        .then((resp) => resp.json)
+        .then((data) => console.log(data));
+}
