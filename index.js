b6c196f4620452bd4609991ae80b00e29c150d5d
Krithik Singh <krithiksingh@gmail.com>
2023-12-01 15:32:34 -0500

    created forEach method on each expense

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function fetchData() {
     fetch('http://localhost:3000/expenses')
         .then((resp) => resp.json())
-        .then((data) => console.log(data));
+        .then((expenses) => expenses.forEach((expense) => updateExpenseList()));
 }
+
+function updateExpenselist() {}
