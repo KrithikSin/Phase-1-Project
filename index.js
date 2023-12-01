138a89fdeababb985df423e38ec814a38c55206a
Krithik Singh <krithiksingh@gmail.com>
2023-12-01 15:43:24 -0500

    grabbed every element from HTML onto the index.js file for accessibility

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('we are connected!!!');
-    fetchData();
+    const balanceDisplay = document.getElementById('balance-display');
+    const expenseForm = document.getElementById('expense-form');
+    const expenseDescriptionInput = document.getElementById(
+        'expense-description'
+    );
+    const expenseAmountInput = document.getElementById('expense-amount');
+    const expenseCategoryInput = document.getElementById('expense-category');
+    const expenseList = document.getElementById('expense-list');
 });
 
 function fetchData() {
     fetch('http://localhost:3000/expenses')
         .then((resp) => resp.json())
-        .then((expenses) => expenses.forEach((expense) => updateExpenseList()));
+        .then((expenses) =>
+            expenses.forEach((expense) => updateExpenseList(expense))
+        );
 }
 
-function updateExpenselist() {}
+function updateExpenseList(expense) {}
